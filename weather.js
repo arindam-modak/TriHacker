a22a9c59a4031fc1d55f511a3136e10cbc664077
Arindam Das Modak <arindam.modak03@gmail.com>
2017-03-26 11:16:06 +0530

    Update weather.js

--- weather.js
+++ weather.js
@@ -1,4 +1,14 @@
 $(document).ready(function(){
+	var sum=0;
+	var num=0;
+	function foo() {
+
+		if( typeof foo.counter == 'undefined' ) {
+			foo.counter = 0;
+		}
+		foo.counter++;
+		document.write(foo.counter+"<br />");
+	}
 	$('#submitWeather').click(function() {
 		var city=$("#city").val();
 		
@@ -9,7 +19,8 @@ $(document).ready(function(){
 				type:"GET",
 				dataType:"jsonp",
 				success: function(data){
-					var widget = show(data);
+					
+					var widget = show(data,sum,num);
 					$("#show").html(widget);
 					$("#city").val('');
 				}
@@ -19,8 +30,12 @@ $(document).ready(function(){
 		}
 	});
 });
-function show(data){
-	return "<h3><strong>Weather</strong>: "+ data.weather[0].main +"</h3>" +
+function show(data,sum,num){
+	sum += data.main.temp;
+	num += 1;
+	
+	return "<h2><strong><u>CITY</u></strong>: "+ $("#city").val() +"</h3>" +
+	"<h3><strong>Weather</strong>: "+ data.weather[0].main +"</h3>" +
 	"<h3><strong>Weather</strong>: "+ data.weather[0].description +"</h3>" +
 	"<h3><strong>Temperature</strong>: "+ data.main.temp +"</h3>" +
 	"<h3><strong>Pressure</strong>: " + data.main.pressure +"</h3>" +
