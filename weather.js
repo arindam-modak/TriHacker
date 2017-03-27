0c8182b376965be4cfcaeb8d5087cfad4cd01953
Arindam Das Modak <arindam.modak03@gmail.com>
2017-03-27 16:15:12 +0530

    Update weather.js

--- weather.js
+++ weather.js
@@ -1,14 +1,5 @@
 $(document).ready(function(){
-	var sum=0;
-	var num=0;
-	function foo() {
-
-		if( typeof foo.counter == 'undefined' ) {
-			foo.counter = 0;
-		}
-		foo.counter++;
-		document.write(foo.counter+"<br />");
-	}
+	
 	$('#submitWeather').click(function() {
 		var city=$("#city").val();
 		
@@ -20,7 +11,7 @@ $(document).ready(function(){
 				dataType:"jsonp",
 				success: function(data){
 					
-					var widget = show(data,sum,num);
+					var widget = show(data);
 					$("#show").html(widget);
 					$("#city").val('');
 				}
@@ -30,18 +21,16 @@ $(document).ready(function(){
 		}
 	});
 });
-function show(data,sum,num){
-	sum += data.main.temp;
-	num += 1;
+function show(data){
 	
 	return "<h2><strong><u>CITY</u></strong>: "+ $("#city").val() +"</h3>" +
-	"<h3><strong>Weather</strong>: "+ data.weather[0].main +"</h3>" +
+	"<h3><strong>Weather</strong>: "+ data.weather[0].main + "</h3>" +
 	"<h3><strong>Weather</strong>: "+ data.weather[0].description +"</h3>" +
-	"<h3><strong>Temperature</strong>: "+ data.main.temp +"</h3>" +
+	"<h3><strong>Temperature</strong>: "+ data.main.temp +" <sup>o</sup>C" + "</h3>" +
 	"<h3><strong>Pressure</strong>: " + data.main.pressure +"</h3>" +
 	"<h3><strong>Humidity</strong>: " + data.main.humidity +"</h3>" +
-	"<h3><strong>Min. Temperature</strong>: " + data.main.temp_min+"</h3>" +
-	"<h3><strong>Max. Temperature</strong>: " + data.main.temp_max +"</h3>" +
-	"<h3><strong>Wind Speed</strong>: " + data.wind.speed+"</h3>" ;
+	"<h3><strong>Min. Temperature</strong>: " + data.main.temp_min + " <sup>o</sup>C" + "</h3>" +
+	"<h3><strong>Max. Temperature</strong>: " + data.main.temp_max + " <sup>o</sup>C" + "</h3>" +
+	"<h3><strong>Wind Speed</strong>: " + data.wind.speed + " Km/h "+"</h3>" ;
 	
 }
