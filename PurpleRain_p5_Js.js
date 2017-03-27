cc10ae2b4e13a53aa707cc47edb8bdcd2bae5b8f
Arindam Das Modak <arindam.modak03@gmail.com>
2017-03-27 16:27:32 +0530

    Update PurpleRain_p5_Js.js

--- PurpleRain_p5_Js.js
+++ PurpleRain_p5_Js.js
@@ -1,7 +1,7 @@
 var drops = [];
                                    //BY - Arindam Das Modak
 function setup() {
-  createCanvas(1310, 60);
+  createCanvas(1310, 58);
   for (var i = 0; i < 500; i++) {
     drops[i] = new Drop();
   }
